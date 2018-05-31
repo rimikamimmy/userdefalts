d302760a6cbdad05a314f114f90ecc7bf50f1e0c
RIMIKA <rimikamimmy727@gmail.com>
2018-05-31 10:26:40 +0900

    追加課題 userdefalt

--- userdefalts/ViewController.swift
+++ userdefalts/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fruitlabel: UILabel!
+    @IBOutlet weak var vegelabel: UILabel!
+    
+    
+    @IBOutlet weak var fruittext: UITextField!
+    @IBOutlet weak var vegetext: UITextField!
+    
+    @IBOutlet weak var save: UIButton!
+    
+    
+    
+    let saveData = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //保存するためのまず読み込むためのコード
+        if saveData.object(forKey: "fruit") != nil{
+            fruittext.text = (saveData.object(forKey: "fruit") as! String)
+        }
+        
+        if saveData.object(forKey: "vegetable") != nil{
+            vegetext.text = (saveData.object(forKey: "vegetable") as! String)
+        }
+   
     }
 
+    //保存ボタン押した後の動作＝保存という行為
+    @IBAction func saveWord(){
+        
+        let fruit :String = fruittext.text!
+        saveData.set(fruit, forKey: "fruit")
+        
+        let vegetable :String = vegetext.text!
+        saveData.set(vegetable, forKey: "vegetable")
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
